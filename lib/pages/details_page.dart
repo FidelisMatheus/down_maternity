7790824e5b599ef679b3e32afb0b46aa94770f80
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-05-21 18:58:45 -0300

    ExpandedWidget modificado
    Modifiquei o expanded widget para receber o model Topic (Tópico)

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,15 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:projeto_sindrome_down/model/topic.dart';
 import 'package:projeto_sindrome_down/routes/routes.dart';
 
 import 'package:projeto_sindrome_down/utils/appcolors.dart';
 import 'package:projeto_sindrome_down/utils/dimensions.dart';
 import 'package:projeto_sindrome_down/widgets/expansion_widget.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
   final int id;
   final String title;
+
   const DetailsScreen({
     Key? key,
     required this.id,
@@ -17,72 +20,112 @@ class DetailsScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  late bool exibir;
+  List<Topic> listTopics = [
+    Topic(
+      'Teste 1',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+    Topic(
+      'Teste 2',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+    Topic(
+      'Teste 2',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+    Topic(
+      'Teste 2',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+    Topic(
+      'Teste 2',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+    Topic(
+      'Teste 2',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      false,
+      false,
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    exibir = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF8ECAE6),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                child: Image.asset('images/detalhes.png'),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(30),
-                      bottomLeft: Radius.circular(30)),
-                ),
-              ),
-              Center(child: ExpansionWidget(title: title)),
-              Container(
-                padding: EdgeInsets.only(left: 5),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.initial);
-                  },
-                  icon: Icon(Icons.arrow_back_ios),
-                ),
-                width: Dimensions.cardHeight50,
-                height: Dimensions.cardWidth50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  child: Image.asset('images/detalhes.png'),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(30), bottomLeft: Radius.circular(30)),
                   ),
-                  color: Colors.white.withOpacity(0.5),
                 ),
-                margin: EdgeInsets.only(
-                  top: Dimensions.height10,
-                  left: Dimensions.height10,
+                Container(
+                  width: 40,
+                  height: 40,
+                  margin: EdgeInsets.only(
+                    right: Dimensions.width350,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Get.toNamed(Routes.homeScreen);
+                    },
+                    icon: Icon(
+                      Icons.chevron_left_rounded,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 30, 10),
-            child: Center(
-              child: Text(
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy  text ever since the 1500s, w hen an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                style: TextStyle(
-                  fontFamily: 'Open Sans',
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18,
-                ),
-              ),
+              ],
             ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 2.0,
-                    offset: Offset(2.0, 5.0),
-                  )
-                ],
-                color: AppColors.yellowColor),
-            width: Dimensions.width350,
-            margin: EdgeInsets.only(top: 30),
-          )
-        ],
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: listTopics.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      listTopics[index].expand = !listTopics[index].expand;
+                    });
+                  },
+                  child: ExpansionWidget(
+                    title: listTopics[index].title,
+                    expand: listTopics[index].expand,
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/expansion_widget.dart
+++ lib/widgets/expansion_widget.dart
@@ -7,9 +7,12 @@ import 'package:projeto_sindrome_down/utils/dimensions.dart';
 
 class ExpansionWidget extends StatefulWidget {
   final String title;
-  const ExpansionWidget({
+  late bool expand;
+
+  ExpansionWidget({
     Key? key,
     required this.title,
+    required this.expand,
   }) : super(key: key);
 
   @override
@@ -19,51 +22,90 @@ class ExpansionWidget extends StatefulWidget {
 class _ExpansionWidgetState extends State<ExpansionWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 350,
-      margin: EdgeInsets.only(top: 255),
-      padding: EdgeInsets.fromLTRB(10, 5, 5, 10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-          // ignore: prefer_const_literals_to_create_immutables
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              blurRadius: 2.0,
-              offset: Offset(2.0, 5.0),
-            )
-          ],
-          color: AppColors.yellowColor),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: Row(
-              children: [
-                Container(
-                  width: 250,
-                  alignment: Alignment.center,
-                  child: Text(
-                    widget.title,
-                    style: TextStyle(
-                      fontFamily: 'Open Sans',
-                      fontWeight: FontWeight.w800,
-                      fontSize: Dimensions.font16,
+    return Column(
+      children: [
+        Container(
+          width: Dimensions.width350,
+          height: 50,
+          decoration: BoxDecoration(
+              borderRadius: widget.expand
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    )
+                  : BorderRadius.all(Radius.circular(8)),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 2.0,
+                  offset: Offset(2.0, 5.0),
+                )
+              ],
+              color: AppColors.yellowColor),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w800,
+                        fontSize: Dimensions.font16,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
+                  ),
+                  Icon(Icons.arrow_drop_down_outlined),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.check_box_outline_blank),
+              ),
+            ],
+          ),
+        ),
+        widget.expand == true
+            ? Container(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac est dignissim, tincidunt quam a, tristique ex. Phasellus leo sem, vehicula et tristique in, tempor ullamcorper nisl. Aliquam semper condimentum justo, vel sollicitudin tellus sagittis in. Sed placerat metus sit amet felis ultrices molestie. ",
+                      style: TextStyle(
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
                   ),
                 ),
-                Icon(Icons.arrow_drop_down_outlined),
-              ],
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.check_box_outline_blank),
-          ),
-        ],
-      ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 2.0,
+                        offset: Offset(2.0, 5.0),
+                      )
+                    ],
+                    color: AppColors.yellowColor),
+                width: Dimensions.width350,
+                margin: EdgeInsets.only(bottom: 20),
+              )
+            : SizedBox(
+                height: 20,
+              )
+      ],
     );
   }
 }
